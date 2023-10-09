d199c0e22e710d0ead308a3ad271ee70c6a3dafe
Claudia Estrada <cdcruz.estrada@gmail.com>
2023-10-08 21:17:37 -0600

    Agregando ejer.3 B2

---
+++ 3.area-triangulo.py
@@ -0,0 +1,12 @@
+'''
+Calcular el área de un triángulo
+1. Crear variable con int para que base y altura.
+2. Crear variable área que haga la operación aritmática.
+2. Imprimir el mensaje con el resultado.
+'''
+
+base = int(input("Escribe la base del triángulo: "))
+altura = int(input("Escribe la altura del triángulo: "))
+area = (base * altura) / 2
+
+print("El área del triángulo es:", area, "cm².")
