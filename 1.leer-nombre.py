7dffffbd54b2a9697380e2e3af49e0e1d10163f4
Claudia Estrada <cdcruz.estrada@gmail.com>
2023-10-08 21:12:17 -0600

    Agregando ejer.1 B2

---
+++ 1.leer-nombre.py
@@ -0,0 +1,8 @@
+'''
+Leer el nombre de la persona e imprimirlo en la pantalla. 
+1. Crear variable para ingresar nombre.
+2. Imprimir ael nombre más el mensaje de hola.
+'''
+
+nombre = input("Ingresa tu nombre: ")
+print("¡Hola " + nombre + "!")
